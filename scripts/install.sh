432057a0022b43fc12fc6789c68dbbc2732075df
Malte Kollasch <maltekollasch2003@gmail.com>
2021-04-11 16:37:37 +0200

    finish the install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
        select opt in $OPTIONS; do
            if [ "$opt" = "Englisch" ]; then
             echo "========= Moonraker-MQTT-plugin - Installation Script ==========="
-            echo "========= Install the Subsystem ========="
+            echo "========= Update the Subsystem ========="
             sudo apt update && sudo apt upgrade -y
             echo "========= Install Curl ========="
             sudo apt install curl
@@ -14,12 +14,26 @@
             sudo pip3 install paho-mqtt
             sudo pip3 install requests
             sudo pip3 install ConfigParser
-            echo "========= Copy the files ========="           
+            echo "========= Copy the Config file ========="           
             cp -i /home/pi/Moonraker-MQTT-plugin/scripts/moonraker_mqtt.cfg /home/pi/klipper_config
             echo "========= Installing successful ========="
             exit
            elif [ "$opt" = "Deutsch" ]; then
-            echo "========= In Arbeit ========="
+            echo "========= Moonraker-MQTT-plugin - Installations Script ==========="
+            echo "========= Update das Betiebssystem ========="
+            sudo apt update && sudo apt upgrade -y
+            echo "========= Installiere Curl ========="
+            sudo apt install curl
+            curl --version
+            echo "========= Installiere pip ========="
+            sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+            sudo python3 get-pip.py
+            sudo pip3 install paho-mqtt
+            sudo pip3 install requests
+            sudo pip3 install ConfigParser
+            echo "========= Kopiere die Config Datei ========="           
+            cp -i /home/pi/Moonraker-MQTT-plugin/scripts/moonraker_mqtt.cfg /home/pi/klipper_config
+            echo "========= Insterllation Abgeschlossen ========="
             exit
            elif [ "$opt" = "Quit" ]; then
             echo done
