33d51f22d63f4e1c1b468a85ace91bd21803ea23
Malte Kollasch <maltekollasch2003@gmail.com>
2021-04-11 16:16:57 +0200

    correct path

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,25 +15,10 @@
             sudo pip3 install requests
             sudo pip3 install ConfigParser
             echo "========= Copy the files ========="           
-            cp -i /home/pi/Moonraker-MQTT-plugin/scripts/Moonraker_mqtt.cfg /home/pi/klipper_config
+            cp -i /home/pi/Moonraker-MQTT-plugin/scripts/moonraker_mqtt.cfg /home/pi/klipper_config
             echo "========= Installing successful ========="
             exit
            elif [ "$opt" = "Deutsch" ]; then
-            echo "========= Moonraker-MQTT-plugin - Installations Script ==========="
-            sudo -s
-            echo "========= Installiere MQTT moonraker Plugin ========="
-            apt update && sudo apt upgrade
-            echo "========= Installiere curl und pip ========="
-            apt install curl
-            curl --version
-            curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-            python3 get-pip.py
-            pip3 install paho-mqtt
-            pip3 install requests
-            pip3 install ConfigParser
-            cp -i /home/pi/Moonraker-MQTT-plugin/scripts/Moonraker_mqtt.cfg /home/pi/klipper_config
-            echo "========= Installieren des Basissystems Erfolgreich Abgeschlossen ========="
-            exit
            elif [ "$opt" = "Quit" ]; then
             echo done
             exit
